819476b710badea58c1ecf41dd9c19bcf5a0f910
ismayax <ismailarfaoui14@gmail.com>
2024-04-12 10:16:51 +0200

    ismail

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Cursosniveles from './components/Cursosniveles';
+import Listaexperimentos from './components/Listaexperimentos';
+import Login from './components/Login';
+import Paginaprincipal from './components/Paginaprincipal';
+
+export default App;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <Cursosniveles/>
+      <Paginaprincipal/>
+      <Listaexperimentos/>
+      <Login/>
 
-export default App;
+    </div>
+  )
+}
