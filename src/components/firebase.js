5881de3612b8f3e6359b9e449f652a87f3ace544
LUIS <lrp0030@alu.medac.es>
2024-04-22 14:47:47 +0200

    firebase implementado

---
+++ src/components/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/database'; // Importa el módulo de base de datos si lo necesitas
+
+const firebaseConfig = {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_AUTH_DOMAIN",
+    databaseURL: "TU_DATABASE_URL",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_STORAGE_BUCKET",
+    messagingSenderId: "TU_MESSAGING_SENDER_ID",
+    appId: "TU_APP_ID"
+};
+
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
