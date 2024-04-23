c16af33d6184a55b95dd8062f2a97cb3f16fd8ef
arrc20 <>
2024-04-23 12:59:52 +0200

    Implementando firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,30 @@
-import React from "react";
-const login = () => {
-    return(
+import React, { useState, useEffect } from "react";
+import db from "./firebaseConfig"; // Importa la instancia de Firestore desde donde la hayas exportado
+
+const Login = () => {
+    const [dato, setDato] = useState("");
+
+    useEffect(() => {
+        const usuariosRef = collection(db, "usuarios");
+        getDocs(usuariosRef)
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    const data = doc.data();
+                    const datoEspecifico = data.nombre_usuario;
+                    setDato(datoEspecifico);
+                });
+            })
+            .catch((error) => {
+                console.log("Error al obtener los datos:", error);
+            });
+    }, []);
+
+    return (
         <div>
-            <h1> HOLA</h1>
+            <h1>¡Hola!</h1>
+            <p>El dato obtenido de Firestore es: {dato}</p>
         </div>
+    );
+};
 
-
-    )
-}
-export default login;
+export default Login;
