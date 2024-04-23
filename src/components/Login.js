77586279c8a06c07690261c7fe6b82965ffad714
LUIS <lrp0030@alu.medac.es>
2024-04-23 19:54:07 +0200

    Implementado Firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from "react";
-import db from "./firebaseConfig"; // Importa la instancia de Firestore desde donde la hayas exportado
+import { collection, getDocs } from "firebase/firestore";
+import db from "./firebaseConfig";
 
 const Login = () => {
-    const [dato, setDato] = useState("");
+    const [nombreUsuario, setNombreUsuario] = useState("");
 
     useEffect(() => {
-        const usuariosRef = collection(db, "usuarios");
-        getDocs(usuariosRef)
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
+        const obtenerDatos = async () => {
+            try {
+                const usuariosRef = collection(db, "centro1"); // Cambia "usuarios" por el nombre de la colección que contiene el documento
+                const snapshot = await getDocs(usuariosRef);
+                snapshot.forEach((doc) => {
                     const data = doc.data();
-                    const datoEspecifico = data.nombre_usuario;
-                    setDato(datoEspecifico);
+                    const nombre = data.nombre_usuario;
+                    setNombreUsuario(nombre);
                 });
-            })
-            .catch((error) => {
-                console.log("Error al obtener los datos:", error);
-            });
+            } catch (error) {
+                console.error("Error al obtener datos:", error);
+            }
+        };
+
+        obtenerDatos();
     }, []);
 
     return (
         <div>
             <h1>¡Hola!</h1>
-            <p>El dato obtenido de Firestore es: {dato}</p>
+            <p>El nombre de usuario obtenido de Firestore es: {nombreUsuario}</p>
         </div>
     );
 };
