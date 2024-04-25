e329ee6c61c9d2365b7de25bccb3346237f88274
LUIS <lrp0030@alu.medac.es>
2024-04-25 16:01:34 +0200

    conexion exitosa

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Listaexperimentos from './components/Listaexperimentos';
 import Login from './components/Login';
 import Paginaprincipal from './components/Paginaprincipal';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Datos from "./components/Datos";
 
 function App() {
   return (
 
-    
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/Paginaprincipal' element={<Paginaprincipal />} />
-        <Route path='/Cursosniveles' element={<Cursosniveles />} />
-        <Route path='/experimentos' element={<Listaexperimentos />} />
-      </Routes>
+    <Datos></Datos>
+    //<Routes>
+    //  <Route path='/' element={<Login />} />
+    //  <Route path='/Paginaprincipal' element={<Paginaprincipal />} />
+    //  <Route path='/Cursosniveles' element={<Cursosniveles />} />
+    //  <Route path='/experimentos' element={<Listaexperimentos />} />
+    //</Routes>
     
   );
 }
