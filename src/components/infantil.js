6145635298d2afb8df3317b6971d9bcfc837d1cf
LUIS <lrp0030@alu.medac.es>
2024-05-09 13:43:47 +0200

    fusion de base de datos

--- src/components/infantil.js
+++ src/components/infantil.js
@@ -7,19 +7,37 @@ const Infantil = () => {
   return (
     <nav>
       <div className="barra">
-      <h2 className="elemonto1">
-          {/* Utiliza Link en lugar de link */}
-          <Link to="/Paginaprincipal">ATRÁS</Link>
-        </h2>
         <h1 className="elemento">SELECCIONE EL CURSO</h1>
       </div>
-      <div className="cuadro-container">
-        <Link to="/experimentos" className="cuadros">4</Link>
-        <Link to="/experimentos" className="cuadros">5</Link>
-        <Link to="/experimentos" className="cuadros">6</Link>
+      <div className="cuadro-container" style={containerStyle}>
+      <Link to="/Listaexperimentos" className="cuadros" style={cuadroStyle}>3</Link>
+      <Link to="/Listaexperimentos" className="cuadros" style={cuadroStyle}>4</Link>
+      <Link to="/Listaexperimentos" className="cuadros" style={cuadroStyle}>5</Link>
       </div>
     </nav>
   );
-}
+};
 
 export default Infantil;
+
+const cuadroStyle = {
+  marginBottom: "50px", // Reducir el espacio inferior
+  width: "30%", // Ancho de los cuadros (ajustar según sea necesario)
+  padding: "50px", // Espaciado interno del cuadro
+  borderRadius: "50px", // Bordes redondeados del cuadro
+  backgroundColor: "rgb(120,168,128)", // Color de fondo del cuadro
+  textAlign: "center", // Alinear el texto al centro
+  display: "inline-block", // Hacer que los cuadros se muestren en línea
+  textDecoration: "none", // Quitar subrayado de los enlaces
+  color: "white", // Color de texto blanco
+  fontSize: "50px", // Tamaño de letra aumentado
+  fontWeight: "bold", // Texto en negrita
+  lineHeight: "100px", // Altura de línea igual a la altura del cuadro
+};
+
+const containerStyle = {
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center", // Centrar los elementos verticalmente
+  height: "100px", // Altura igual a la altura de la ventana
+};
