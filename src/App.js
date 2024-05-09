84be561c2aba493ef24c719c88da5a82f67e6d29
Zhir4yr <zhirayrhash@gmail.com>
2024-05-09 14:13:09 +0200

    Experimentos conFlechas

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Cursosniveles from './components/Cursosniveles';
-import Listaexperimentos from './components/Listaexperimentos';
+import Listaexperimentosdatos from './components/Listaexperimentosdatos';
+import Experimentos from './components/Experimentos';
 import Login from './components/Login';
 import Paginaprincipal from './components/Paginaprincipal';
+import Cursosniveles from './components/Cursosniveles';
 import Infantil from './components/infantil';
 import Secundaria from './components/Secundaria';
 import Registro from './components/Registrarse';
-import Contrasena from './components/OlvidoContra';  
-import Listaexperimentosdatos from "./components/Listaexperimentosdatos";
-import DetalleExperimento from "./components/DetalleExperimento";
+import Contrasena from './components/OlvidoContra';
+
+
 
 function App() {
   return (
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/Paginaprincipal' element={<Paginaprincipal />} />
-        <Route path='/Cursosniveles' element={<Cursosniveles />} />
-        <Route path='/experimentos' element={<Listaexperimentosdatos />} />
-        <Route path='/infantil' element={<Infantil />} />
-        <Route path='/secundaria' element={<Secundaria />} />
-        <Route path='/registro' element={<Registro />} />
-        <Route path='/contrasena' element={<Contrasena />} />  // Ruta para el componente de olvido de contraseña
-        <Route path='/contrasena' element={<Contrasena />} />
-        <Route path='/actividades/:id' element={<DetalleExperimento />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Paginaprincipal" element={<Paginaprincipal />} />
+        <Route path="/Cursosniveles" element={<Cursosniveles />} />
+        <Route path="/experimentos" element={<Listaexperimentosdatos />} />
+        <Route path="/infantil" element={<Infantil />} />
+        <Route path="/secundaria" element={<Secundaria />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/contrasena" element={<Contrasena />} />
+        <Route path="/actividades/:id" element={<Experimentos />} />
       </Routes>
   );
 }
