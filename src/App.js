de790acfa4427a35cf48329b1bb948a879c70053
LUIS <lrp0030@alu.medac.es>
2024-05-14 13:09:26 +0200

    app Infantil

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Listaexperimentos from './components/Listaexperimentos'; // Importa el componente Listaexperimentos
 import Login from './components/Login';
 import Paginaprincipal from './components/Paginaprincipal';
-import Infantil from './components/infantil';
 import Secundaria from './components/Secundaria';
 import Registro from './components/Registrarse';
 import Contrasena from './components/OlvidoContra';  
 import DetalleExperimento from "./components/DetalleExperimento";
 import PlanesSuscripcion from './components/PlanesSuscripcion';
 import Pago from './components/pago';
+import Infantil from './components/Infantil';
 import Primaria from './components/Primaria';
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
         <Route path='/Paginaprincipal' element={<Paginaprincipal />} />
         <Route path='/Primaria' element={<Primaria />} />
         <Route path='/Listaexperimentos/grupo/:grupo' element={<Listaexperimentos />} /> 
-        <Route path='/infantil' element={<Infantil />} />
+        <Route path='/Infantil' element={<Infantil />} />
         <Route path='/secundaria' element={<Secundaria />} />
         <Route path='/registro' element={<Registro />} />
         <Route path='/contrasena' element={<Contrasena />} />  // Ruta única para el componente de olvido de contraseña
