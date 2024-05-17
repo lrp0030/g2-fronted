0d1d1040369d8770afc1f959aee0fe5c3f1b7cd2
Zhir4yr <zhirayrhash@gmail.com>
2024-05-17 09:19:12 +0200

    Desarrollo detalles experimento

--- src/components/Experimentos.js
+++ src/components/Experimentos.js
@@ -4,11 +4,24 @@ import { doc, getDoc } from 'firebase/firestore';
 import db from './firebaseConfig';
 import styles from "../assets/css/Experimento.module.css"; 
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css"; 
+import ejemploImage from '../assets/img/ejemplo.jpg';
+
 
 function Experimento() {
   const [experimento, setExperimento] = useState(null);
   const [pasoActual, setPasoActual] = useState(0);
-  const { id } = useParams(); // Asegúrate de que el nombre del parámetro coincide con el configurado en tu ruta
+  const { id } = useParams();
+  //Configuración para el carrousel de fotos
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  }
 
   useEffect(() => {
     const fetchExperimento = async () => { 
@@ -16,10 +29,10 @@ function Experimento() {
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data()); // Depuración para ver los datos obtenidos
+        console.log("Document data:", docSnap.data());
         setExperimento({ id: docSnap.id, ...docSnap.data() });
       } else {
-        console.log("No such document!"); // Informa si el documento no existe
+        console.log("No such document!");
       }
     };
 
@@ -44,23 +57,61 @@ function Experimento() {
 
   return (
     <div className={styles.experimentoContainer}>
-        <h1 className={styles.experimentoTitle}>{experimento?.titulo}</h1>
-        <p className={styles.experimentoDescription}>{experimento?.descripcion}</p>
-        <h2 className={styles.materialsHeader}>Materiales necesarios:</h2>
-        <ul className={styles.materialList}>
-            {experimento?.materiales.map(material => (
-                <li key={material} className={styles.materialItem}>{material}</li>
-            ))}
+      <div className={styles.header}>
+        <h1 className={styles.experimentoTitle}>{experimento.titulo}</h1>
+        <span className={styles.grupo}>{experimento.grupo}</span>
+      </div>
+      <p className={styles.experimentoDescription}>{experimento.descripcion_actividad}</p>
+      <div className={styles.hipotesis}>
+        <h3>Preguntas Hipotéticas Iniciales:</h3>
+        <ul>
+          {experimento.preguntas_iniciales_hipotesis.map(pregunta => (
+            <li key={pregunta}>{pregunta}</li>
+          ))}
         </ul>
-        <div className={styles.controls}>
-            <button className={styles.controlButton} onClick={pasoAnterior} disabled={pasoActual === 0}>
-                <FaArrowLeft /> Anterior
-            </button>
-            <p>Paso {pasoActual + 1}: {experimento?.Pasos[pasoActual]}</p>
-            <button className={styles.controlButton} onClick={siguientePaso} disabled={pasoActual === experimento.Pasos.length - 1}>
-                Siguiente <FaArrowRight />
-            </button>
-        </div>
+      </div>
+      <h2 className={styles.materialsHeader}>Materiales necesarios:</h2>
+      <ul className={styles.materialList}>
+        {experimento.materiales.map(material => (
+          <li key={material} className={styles.materialItem}>{material}</li>
+        ))}
+      </ul> 
+      <div className={styles.controls}>
+        <button className={styles.controlButton} onClick={pasoAnterior} disabled={pasoActual === 0}>
+          <FaArrowLeft /> Anterior
+        </button>
+        <div className={styles.carouselContainer}>
+        <Slider {...settings}>
+          <div>
+            <img src={ejemploImage} alt="Experimento" />
+          </div>
+          <div>
+            <img src={ejemploImage} alt="Experimento" />
+          </div>
+          <div>
+            <img src={ejemploImage} alt="Experimento" />
+          </div>
+          <div>
+            <img src={ejemploImage} alt="Experimento" />
+          </div>
+        </Slider>
+      </div>
+        <p>Paso {pasoActual + 1}: {experimento.Pasos[pasoActual]}</p>
+        <button className={styles.controlButton} onClick={siguientePaso} disabled={pasoActual === experimento.Pasos.length - 1}>
+          Siguiente <FaArrowRight />
+        </button>
+      </div>
+      <div className={styles.conclusion}>
+        <h3>Preguntas Finales para Conclusión:</h3>
+        <ul>
+          {experimento.preguntas_finales_conclusion.map(pregunta => (
+            <li key={pregunta}>{pregunta}</li>
+          ))}
+        </ul>
+      </div>
+      <p className={styles.explicacion}>{experimento.explicacion}</p>
+
+    
     </div>
   );
 }
