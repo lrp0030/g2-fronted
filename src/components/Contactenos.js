df93d619456539abd0c415ad0bebdeaa685889fc
Zhir4yr <zhirayrhash@gmail.com>
2024-05-21 10:43:18 +0200

    Arreglo mensaje Contactenos para que se envie el contenido al correo de la empresa.

--- src/components/Contactenos.js
+++ src/components/Contactenos.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
-import "../assets/css/contactenos.css"; 
+import "../assets/css/contactenos.css";
+
 function Contactenos() {
   const [formData, setFormData] = useState({
-    nombre: '',
-    email: '',
-    telefono: '',
-    comentario: ''
+    from_name: '',
+    from_email: '',
+    from_phone: '',
+    message: ''
   });
+  const [mensajeExito, setMensajeExito] = useState('');
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -17,61 +19,68 @@ function Contactenos() {
     e.preventDefault();
     emailjs.sendForm('service_388zi39', 'template_a91d9nf', e.target, 'tLr9WocZl0HqlGnLg')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
+        setMensajeExito('Enviado correctamente, te contactaremos pronto!');
+        setFormData({
+          from_name: '',
+          from_email: '',
+          from_phone: '',
+          message: ''
+        });
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
-    e.target.reset(); // Reset the form fields after submission
   };
 
   return (
     <div className="contact-container">
-      <h1>Contactenos</h1>
+      <h1>Contáctenos</h1>
       <form className="contact-form" onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="nombre">Nombre:</label>
+          <label htmlFor="from_name">Nombre:</label>
           <input
             type="text"
-            id="nombre"
-            name="nombre"
-            value={formData.nombre}
+            id="from_name"
+            name="from_name"
+            value={formData.from_name}
             onChange={handleChange}
             required
           />
         </div>
         <div className="form-group">
-          <label htmlFor="email">Correo Electrónico:</label>
+          <label htmlFor="from_email">Correo Electrónico:</label>
           <input
             type="email"
-            id="email"
-            name="email"
-            value={formData.email}
+            id="from_email"
+            name="from_email"
+            value={formData.from_email}
             onChange={handleChange}
             required
           />
         </div>
         <div className="form-group">
-          <label htmlFor="telefono">Número de Teléfono:</label>
+          <label htmlFor="from_phone">Número de Teléfono:</label>
           <input
             type="tel"
-            id="telefono"
-            name="telefono"
-            value={formData.telefono}
+            id="from_phone"
+            name="from_phone"
+            value={formData.from_phone}
             onChange={handleChange}
             required
           />
         </div>
         <div className="form-group">
-          <label htmlFor="comentario">Comentario:</label>
+          <label htmlFor="message">Comentario:</label>
           <textarea
-            id="comentario"
-            name="comentario"
-            value={formData.comentario}
+            id="message"
+            name="message"
+            value={formData.message}
             onChange={handleChange}
             required
           ></textarea>
         </div>
         <button type="submit" className="submit-button">Enviar</button>
+        {mensajeExito && <p className="success-message">{mensajeExito}</p>}
       </form>
     </div>
   );
